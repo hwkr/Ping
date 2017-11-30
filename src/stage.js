4a9040ade69c4e5900e2e6f677d507e8f749c5e3
Brynn Hawker <brynn@hawker.me>
2017-11-29 22:55:54 -0800

    Hit from top only

--- src/stage.js
+++ src/stage.js
@@ -56,6 +56,8 @@ const game = new Phaser.Game(
       layers.decorations = map.createLayer('Decoration');
 
       map.setCollisionBetween(1, 1000, true, layers.ground);
+      setTileCollision(layers.ground, [ 262, 366, 368, 370 ], { top: true, bottom: false, left: false, right: false });
+
       layers.background.resizeWorld();
 
       players = game.add.group();
@@ -80,7 +82,7 @@ const game = new Phaser.Game(
 
 
 function addPlayer(playerId) {
-  const player = game.add.sprite(game.world.width / 2, game.world.height - 100, 'dude');
+  const player = game.add.sprite(game.world.width / 2, game.world.height - 300, 'dude');
 
   player.id = playerId;
   player.power = 10;
@@ -109,6 +111,50 @@ function movePlayer(playerId, direction) {
   if (direction === 'left') player.body.velocity.x = -10*player.power;
   if (player.body.onFloor())
   {
-    player.body.velocity.y = -15*player.power;
+    player.body.velocity.y = -20*player.power;
   }
 }
+
+function setTileCollision(mapLayer, idxOrArray, dirs) {
+
+  var mFunc; // tile index matching function
+  if (idxOrArray.length) {
+      // if idxOrArray is an array, use a function with a loop
+      mFunc = function(inp) {
+          for (var i = 0; i < idxOrArray.length; i++) {
+              if (idxOrArray[i] === inp) {
+                  return true;
+              }
+          }
+          return false;
+      };
+  } else {
+      // if idxOrArray is a single number, use a simple function
+      mFunc = function(inp) {
+          return inp === idxOrArray;
+      };
+  }
+
+  // get the 2-dimensional tiles array for this layer
+  var d = mapLayer.map.layers[mapLayer.index].data;
+
+  for (var i = 0; i < d.length; i++) {
+      for (var j = 0; j < d[i].length; j++) {
+          var t = d[i][j];
+          if (mFunc(t.index)) {
+
+              t.collideUp = dirs.top;
+              t.collideDown = dirs.bottom;
+              t.collideLeft = dirs.left;
+              t.collideRight = dirs.right;
+
+              t.faceTop = dirs.top;
+              t.faceBottom = dirs.bottom;
+              t.faceLeft = dirs.left;
+              t.faceRight = dirs.right;
+
+          }
+      }
+  }
+ }
+
